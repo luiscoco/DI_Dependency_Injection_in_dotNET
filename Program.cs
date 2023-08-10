e64c0df237b7b0abb12b7baa1dd14ce7793c8fb1
Luis Coco Enríquez <LEnriquez@luxoft.com>
2023-08-10 11:09:55 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using DI_Dependency_Injection_in_dotNET;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Step 1: Create a service collection
+        var services = new ServiceCollection();
+
+        // Step 2: Register services
+        services.AddTransient<IWeatherService, WeatherService>();
+        services.AddSingleton<IOutputService, ConsoleOutputService>();
+        services.AddScoped<WeatherReporter>();
+
+        // Step 3: Build the service provider
+        var serviceProvider = services.BuildServiceProvider();
+
+        // Step 4: Resolve and use services
+        var weatherReporter = serviceProvider.GetRequiredService<WeatherReporter>();
+        weatherReporter.ReportWeather();
+
+        // Dispose of the service provider when done
+        serviceProvider.Dispose();
+    }
+}
---
+++ Services.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DI_Dependency_Injection_in_dotNET
+{
+    // Services
+
+    public interface IWeatherService
+    {
+        string GetWeather();
+    }
+
+    public class WeatherService : IWeatherService
+    {
+        public string GetWeather()
+        {
+            return "It's sunny today!";
+        }
+    }
+
+    public interface IOutputService
+    {
+        void Write(string message);
+    }
+
+    public class ConsoleOutputService : IOutputService
+    {
+        public void Write(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    // Application
+
+    public class WeatherReporter
+    {
+        private readonly IWeatherService _weatherService;
+        private readonly IOutputService _outputService;
+
+        public WeatherReporter(IWeatherService weatherService, IOutputService outputService)
+        {
+            _weatherService = weatherService;
+            _outputService = outputService;
+        }
+
+        public void ReportWeather()
+        {
+            var weather = _weatherService.GetWeather();
+            _outputService.Write(weather);
+        }
+    }
+}
